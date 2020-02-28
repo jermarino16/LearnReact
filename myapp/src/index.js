7b2d3c085bee6bf815d585de637d0143af9704d3
Jeremy Marino <jermarino16@gmail.com>
2020-02-27 19:21:47 -0800

    are able to reference variables in JSX

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,8 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText() {
+  return "Click on me!";
+}
+
 //	Create a react component
 const App = () => {
+  const buttonText = "Click Me!";
+
   return (
     <div>
       <label className="label" for="name">
@@ -11,7 +17,7 @@ const App = () => {
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        Submit
+        {getButtonText()}
       </button>
     </div>
   );
