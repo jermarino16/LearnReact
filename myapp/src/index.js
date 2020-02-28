b38615e4216edb8398096377b7f4747077e8ce60
Jeremy Marino <jermarino16@gmail.com>
2020-02-27 19:20:02 -0800

    practice using JSX

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,14 +1,21 @@
 //	Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //	Create a react component
 const App = () => {
-	return <div> Hi there! </div>;
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter Name
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show  it on the screen
-ReactDOM.render(
-	<App />,
-	document.querySelector('#root')
-	);
+ReactDOM.render(<App />, document.querySelector("#root"));
