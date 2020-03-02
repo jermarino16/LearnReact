be5727391b75f154edac4d7a2bd492517682fdc6
Jeremy Marino <jermarino16@gmail.com>
2020-03-02 14:46:02 -0800

    added error message

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
+  constructor(props) {
+    super(props);
 
-    return <div>Lattitude: </div>;
+    this.state = { lat: null, errorMessage: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  //React says we have to define render!
+  render() {
+    return (
+      <div>
+        Lattitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
