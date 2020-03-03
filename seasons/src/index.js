9e6b96408f0c1ac916b96d8640cf396c64685e58
Jeremy Marino <jermarino16@gmail.com>
2020-03-02 17:30:57 -0800

    dont have multiple returns in render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -17,8 +17,7 @@ class App extends React.Component {
     console.log("component was updated - it rerender");
   }
 
-  //React says we have to define render!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div> Error: {this.state.errorMessage}</div>;
     }
@@ -26,6 +25,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  //React says we have to define render!
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
